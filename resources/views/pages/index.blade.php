b43226dab27ed104422e1b3b98f5ecb1be27d00e
Hsuan19971005 <hsuan9705@gmail.com>
2024-02-19 00:47:45 +0800

    Upload home page ads image

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,63 +3,11 @@
         @include('pages.header-nav-link')
     </x-slot>
 
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            {{-- carousel --}}
-            {{-- <div class="w-full carousel mb-7">
-                <div id="slide1" class="relative w-full carousel-item">
-                  <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
-                  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" class="btn btn-circle">❮</a>
-                    <a href="#slide2" class="btn btn-circle">❯</a>
-                  </div>
-                </div>
-                <div id="slide2" class="relative w-full carousel-item">
-                  <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
-                  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" class="btn btn-circle">❮</a>
-                    <a href="#slide3" class="btn btn-circle">❯</a>
-                  </div>
-                </div>
-                <div id="slide3" class="relative w-full carousel-item">
-                  <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
-                  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" class="btn btn-circle">❮</a>
-                    <a href="#slide4" class="btn btn-circle">❯</a>
-                  </div>
-                </div>
-                <div id="slide4" class="relative w-full carousel-item">
-                  <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
-                  <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" class="btn btn-circle">❮</a>
-                    <a href="#slide1" class="btn btn-circle">❯</a>
-                  </div>
-                </div>
-            </div> --}}
-            {{-- cards --}}
-            {{-- <div class="flex flex-wrap justify-around ">
-                <div class="mx-4 mb-5 shadow-xl w-72 card card-compact bg-base-100">
-                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                    </div>
-                </div>
-                <div class="mx-4 mb-5 shadow-xl w-72 card card-compact bg-base-100">
-                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                    </div>
-                </div>
-                <div class="mx-4 mb-5 shadow-xl w-72 card card-compact bg-base-100">
-                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="card-title">Shoes!</h2>
-                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                    </div>
-                </div>
-            </div> --}}
+    <div class="max-w-6xl mx-auto">
+        <img src="{{ asset('/image/ad1.jpg') }}" alt="ad">
+        <div class="relative">
+          <img src="{{ asset('/image/ad2.jpg') }}" alt="ad">
+          <a href="{{ route('order.index') }}" target="_blank" class="absolute px-5 py-2 text-lg italic font-medium rounded-lg shadow-md sm:px-10 sm:py-5 sm:text-3xl top-1/3 left-1/2 text-amber-900 hover:from-orange-400 hover:to-amber-200 bg-gradient-to-b to-orange-400 from-amber-200">線上訂購</a>
         </div>
     </div>
 </x-page-layout>
